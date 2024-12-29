f9d8d8fad4a4fd7adde904e1329c1e3d8ed7df2d
ino <87593133+inoUwU@users.noreply.github.com>
2024-12-30 00:22:24 +0900

    add swagger api route

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,13 +2,26 @@ package main
 
 import (
 	"encoding/json"
+	"golang-simple-rest/internal/swagger"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/api/v1/spec", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html")
+		w.Write([]byte(swagger.IndexHTML(r.URL.Host)))
+	})
+
+	mux.HandleFunc("/api/v1/spec/swagger.yml", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain")
+		w.Write([]byte(swagger.SwaggerYAML()))
+	})
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(map[string]any{"message": "Hello"})
 	})
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8080", mux)
 }
